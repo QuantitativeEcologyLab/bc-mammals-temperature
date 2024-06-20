8c635448f19db074116e418647c42ba08f51f59b
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-06-20 15:25:25 -0600

    updated the function and added explanations

--- functions/detrend_speeds.R
+++ functions/detrend_speeds.R
@@ -21,24 +21,42 @@ detrend_speeds <- function(DATA, CTMM, units = FALSE) {
     EST.NULL <- speeds(CTMM, t = DATA$t, fast = TRUE, level = NULL,
                        units = units)
     
-    # reduce DOF by that of null distribution
+    # reduce DOF by the DOF of the null (i.e., mean) speed to reduce the
+    # weight of speed estimates with little to no information on speed.
     DOF <- EST$DOF - EST.NULL$DOF
-    all(DOF>0) # check for validity... always has held so far
     DOF <- pmax(0,DOF) # haven't needed this, but just in case
     
-    S2 <- EST$speed^2 + EST$VAR
+    # assuming a chi^2 likelihood estimation, remove the background speed
+    # in an amount proportional to the null DOF (since the sum of squared
+    # chi random variables follows a chi^2 distribution)
+    # formula used is equivalent to 
+    #' `speed()^2 + d0 / ds * (speed()^2 - null^2)`
+    #' where:
+    #' - `speed()` is the output from `speed()`,
+    #' - `null` is the null speed (mean speed not informed by nearby data), 
+    #' - `d0` is the null DOF for the null speed
+    #' - `ds` is the DOF for the data-informed speed
+    #' this formula increases the speed if `speed() > null` and reduces it
+    #' otherwise (which should keep an average of `null`). The amount by
+    #' which it changes the speed depends on how informed the estimate is,
+    #' based on `d0 / ds`.
+    X2 <- (EST$DOF * EST$speed^2 - EST.NULL$DOF * EST.NULL$speed^2) / DOF
+    X2 <- pmax(X2,0)
     
-    AVE <- (EST$DOF*S2)/DOF
-    
-    # Calculate CIs
-    CI <- sapply(
-      1:length(DATA$t),
-      function(i){ ctmm:::chisq.ci(MLE = AVE[i], DOF = DOF[i],
-                                   level = 0.95, robust = TRUE)})
+    # Calculate chi-squared CIs
+    CI <- sapply(1:length(DATA$t),
+                 function(i){
+                   ctmm:::chisq.ci(MLE = X2[i], DOF = DOF[i],
+                                   level = 0.95, robust = TRUE)
+                 })
+
+    # convert to chi distribution: sqrt(chi^2(n)) ~ chi(n)
     CI <- sqrt(CI)
     
+    # transpose to a readable format
     SPEEDS <- as.data.frame(t(CI))
     
+    # add timestamps
     SPEEDS$time <- DATA$timestamp
   }
   return(SPEEDS)
