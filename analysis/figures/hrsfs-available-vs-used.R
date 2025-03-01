a5d2319662316ce01723bf423b6e197b2d2cb5e9
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-03-01 09:43:20 -0800

    increased nbins for a/u; added to appendix

--- analysis/figures/hrsfs-available-vs-used.R
+++ analysis/figures/hrsfs-available-vs-used.R
@@ -10,33 +10,31 @@ ANIMALS <- readr::read_csv('data/tracking-data/telemetry-metadata.csv',
   pull(animal)
 
 d <- readRDS('data/tracking-data/rsf-data.rds') %>%
-  filter(animal %in% ANIMALS)
-
-a_u <- d %>%
+  filter(animal %in% ANIMALS) %>%
   mutate(elevation_m = elevation_m / 1e3,
          dist_water_m = dist_water_m / 1e3) %>%
   pivot_longer(c(forest_perc, elevation_m, dist_water_m),
                names_to = 'variable') %>%
   mutate(
-    lab = case_when(species == SPECIES[1] ~ SPECIES_LABS[1],
-                        species == SPECIES[2] ~ SPECIES_LABS[2],
-                        species == SPECIES[3] ~ SPECIES_LABS[3],
-                        species == SPECIES[4] ~ SPECIES_LABS[4],
-                        species == SPECIES[5] ~ SPECIES_LABS[5],
-                        species == SPECIES[6] ~ SPECIES_LABS[6],
-                        species == SPECIES[7] ~ SPECIES_LABS[7]),
+    lab = case_when(species == 'Rangifer tarandus southern mountain' ~ SPECIES_LABS[1],
+                    species == 'Ursus arctos horribilis' ~ SPECIES_LABS[2],
+                    species == 'Puma concolor' ~ SPECIES_LABS[3],
+                    species == 'Cervus canadensis' ~ SPECIES_LABS[4],
+                    species == 'Rangifer tarandus boreal' ~ SPECIES_LABS[5],
+                    species == 'Canis lupus' ~ SPECIES_LABS[6],
+                    species == 'Oreamnos americanus' ~ SPECIES_LABS[7]),
     variable =
       case_when(variable == 'forest_perc' ~ "bold(Forest~cover~'(%)')",
                 variable == 'elevation_m' ~ "bold(Elevation~'(km)')",
                 variable == 'dist_water_m' ~ "bold(Distance~from~water~'(km)')") %>%
-      factor(levels = c("bold(Forest~cover~'(%)')",
-                        "bold(Elevation~'(km)')",
-                        "bold(Distance~from~water~'(km)')"))) %>%
-  ggplot() +
+      factor(., levels = unique(.)))
+
+a_u <-
+  ggplot(d) +
   facet_grid(lab ~ variable, scales = 'free', labeller = label_parsed,
              switch = 'x') +
   geom_histogram(aes(value, fill = factor(detected)),
-                 position = 'identity', alpha = 0.5, bins = 10) +
+                 position = 'identity', alpha = 0.5, bins = 30) +
   scale_fill_brewer(NULL, type = 'qual', palette = 6,
                     labels = c('Available', 'Used')) +
   scale_y_continuous(transform = 'sqrt') +
@@ -47,31 +45,12 @@ a_u <- d %>%
 ggsave('figures/available-vs-used.png',
        a_u, width = 16, height = 17, dpi = 600)
 
-a_u_w <- d %>%
-  mutate(elevation_m = elevation_m / 1e3,
-         dist_water_m = dist_water_m / 1e3) %>%
-  pivot_longer(c(forest_perc, elevation_m, dist_water_m),
-               names_to = 'variable') %>%
-  mutate(
-    species = case_when(species == SPECIES[1] ~ SPECIES_LABS[1],
-                        species == SPECIES[2] ~ SPECIES_LABS[2],
-                        species == SPECIES[3] ~ SPECIES_LABS[3],
-                        species == SPECIES[4] ~ SPECIES_LABS[4],
-                        species == SPECIES[5] ~ SPECIES_LABS[5],
-                        species == SPECIES[6] ~ SPECIES_LABS[6],
-                        species == SPECIES[7] ~ SPECIES_LABS[7]),
-    variable =
-      case_when(variable == 'forest_perc' ~ "bold(Forest~cover~'(%)')",
-                variable == 'elevation_m' ~ "bold(Elevation~'(km)')",
-                variable == 'dist_water_m' ~ "bold(Distance~from~water~'(km)')") %>%
-      factor(levels = c("bold(Forest~cover~'(%)')",
-                        "bold(Elevation~'(km)')",
-                        "bold(Distance~from~water~'(km)')"))) %>%
-  ggplot() +
-  facet_grid(species ~ variable, scales = 'free', labeller = label_parsed,
+a_u_w <-
+  ggplot(d) +
+  facet_grid(lab ~ variable, scales = 'free', labeller = label_parsed,
              switch = 'x') +
   geom_histogram(aes(value, fill = factor(detected), weight = weight),
-                 position = 'identity', alpha = 0.5, bins = 10) +
+                 position = 'identity', alpha = 0.5, bins = 30) +
   scale_fill_brewer(NULL, type = 'qual', palette = 6,
                     labels = c('Available', 'Used')) +
   scale_y_continuous(transform = 'sqrt') +
