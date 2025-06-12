b3e8d017468abb80d0546fe9c2db389d469a3fac
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-06-12 15:45:03 -0700

    added argument for legend position

--- functions/get_legend.R
+++ functions/get_legend.R
@@ -2,7 +2,8 @@ library('ggplot2')
 library('cowplot')
 
 #' `get_legend()` for `{cowplot}` version 1.1.3 returns an empty plot 
-get_legend <- function(.plot) {
-  get_plot_component(.plot + theme(legend.position = 'top'),
-                     pattern = 'guide-box-top', return_all = TRUE)
+get_legend <- function(.plot, position = 'top') {
+  get_plot_component(.plot + theme(legend.position = position),
+                     pattern = paste0('guide-box-', position),
+                     return_all = TRUE)
 }
