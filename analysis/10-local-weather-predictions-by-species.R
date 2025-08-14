85d1dcf423135fe3c62a40ea442210d8b8be38aa
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-08-13 17:36:40 -0700

    forgot to push script for new weather projections

--- analysis/10-local-weather-predictions-by-species.R
+++ analysis/10-local-weather-predictions-by-species.R
@@ -20,89 +20,51 @@ ANIMALS <- readr::read_csv('data/tracking-data/telemetry-metadata.csv',
   pull(animal)
 
 # get the extent of the range resident animals using they 99.9% UDs
-uds <-
-  readRDS('models/movement-models-akdes-2024-06-06.rds') %>%
-  group_by(species) %>%
-  # remove animals that are not range resident (invalid weights)
-  filter(animal %in% ANIMALS) %>%
-  mutate(species = if_else(grepl('Rangifer', dataset_name),
-                           dataset_name,
-                           species),
-         species = stringr::str_replace_all(species, '_', ' ') %>%
-           factor(),
-         # get 99.9% UDs
-         akde = map(akde, \(.a) {
-           SpatialPolygonsDataFrame.UD(.a, level.UD = 0.999, level = 0) %>%
-             st_as_sf() %>%
-             filter(grepl('est', name)) %>%
-             st_geometry() %>%
-             st_set_crs(.a@info$projection) %>%
-             st_transform('EPSG:4326') %>%
-             st_as_sf()
-         })) %>%
-  # join within each species
-  select(species, akde) %>%
-  nest(akdes = ! species) %>%
-  mutate(akdes = map(akdes, \(.a) {
-    bind_rows(.a[[1]]) %>%
-      st_make_valid() %>%
-      st_union() %>%
-      st_as_sf() 
-  }))
-
-# get resources for each species
-if(FALSE) {
-  res(rast('data/resource-rasters/forest.tif'))
-  res(rast('data/resource-rasters/bc-dem-z6.tif')) # higher res than others
-  res(rast('data/resource-rasters/bc-distance-from-water.tif'))
+# ctmm is currently not installed on the EME linux because it needs more
+# linux dependencies
+if(file.exists('data/species-uds-99.9-percent.rds')) {
+  uds <- readRDS('data/species-uds-99.9-percent.rds')
+} else {
+  uds <-
+    readRDS('models/movement-models-akdes-2024-06-06.rds') %>%
+    group_by(species) %>%
+    # remove animals that are not range resident (invalid weights)
+    filter(animal %in% ANIMALS) %>%
+    mutate(species = if_else(grepl('Rangifer', dataset_name),
+                             dataset_name,
+                             species),
+           species = stringr::str_replace_all(species, '_', ' ') %>%
+             factor(),
+           # get 99.9% UDs
+           akde = map(akde, \(.a) {
+             SpatialPolygonsDataFrame.UD(.a, level.UD = 0.999, level = 0) %>%
+               st_as_sf() %>%
+               filter(grepl('est', name)) %>%
+               st_geometry() %>%
+               st_set_crs(.a@info$projection) %>%
+               st_transform('EPSG:4326') %>%
+               st_as_sf()
+           })) %>%
+    # join within each species
+    select(species, akde) %>%
+    nest(akdes = ! species) %>%
+    mutate(akdes = map(akdes, \(.a) {
+      bind_rows(.a[[1]]) %>%
+        st_make_valid() %>%
+        st_union() %>%
+        st_as_sf() 
+    }))
+  saveRDS(uds, 'data/species-uds-99.9-percent.rds')
 }
 
-resources <- transmute(
-  uds,
-  species,
-  r = map(akdes, \(.a) {
-    forest <- rast('data/resource-rasters/forest.tif') %>%
-      crop(.a) %>%
-      mask(vect(.a))
-    elev_m <- rast('data/resource-rasters/bc-buffered-dem-z6.tif') %>%
-      project(forest)
-    dist_w <- rast('data/resource-rasters/distance-from-water.tif') %>%
-      project(forest)
-    
-    resources <-
-      left_join(as.data.frame(forest, xy = TRUE) %>%
-                  rename(forest_perc = 3),
-                as.data.frame(elev_m, xy = TRUE) %>%
-                  rename(elevation_m = 3),
-                by = c('x', 'y')) %>%
-      left_join(as.data.frame(dist_w, xy = TRUE) %>%
-                  rename(dist_water_m = 3),
-                by = c('x', 'y'))
-  }))
-resources
-
-resources %>%
-  unnest(r) %>%
-  ggplot(aes(x, y, fill = forest_perc)) +
-  facet_wrap(~ species, scales = 'free') +
-  geom_raster() +
-  labs(x = NULL, y = NULL) +
-  scale_fill_gradient('Forest (%)', low = 'beige', high = 'forestgreen',
-                      na.value = 'grey')
-
-# extract temperatures for each species
-temps <-
-  readRDS('H:/GitHub/bc-mammals-temperature/data/weather-projections.rds')
-
-gc() # clean up
-
+# find unique locations in BC
 unique_locs <-
-  readRDS('data/climate-yearly-projections-2024-06-11.rds') %>%
+  readRDS('data/climate-yearly-projections-2025-2100-only-2025-01-21.rds') %>%
   transmute(long = longitude, lat = latitude) %>%
-  group_by(long, lat) %>%
-  slice(1) %>%
-  ungroup()
+  slice(1, .by = c(long, lat))
+plot(unique_locs)
 
+# find unique BC locations within each species' observed range
 unique_locs_spp <- mutate(
   uds,
   subset = map(akdes, \(.ud) {
@@ -110,12 +72,11 @@ unique_locs_spp <- mutate(
       transmute(
         long, lat, # drop all other columns
         inside =
-          st_intersects(
-            unique_locs %>%
-              st_as_sf(coords = c('long', 'lat')) %>%
-              st_set_crs('EPSG:4326'),
-            .ud,
-            sparse = TRUE) %>%
+          st_intersects(unique_locs %>%
+                          st_as_sf(coords = c('long', 'lat')) %>%
+                          st_set_crs('EPSG:4326'),
+                        .ud,
+                        sparse = TRUE) %>%
           as.numeric(),
         inside = if_else(inside > 0, TRUE, FALSE, missing = FALSE)) %>%
       filter(inside) %>%
@@ -124,31 +85,36 @@ unique_locs_spp <- mutate(
   select(! akdes) %>%
   unnest(subset)
 
-ggplot(unique_locs_spp, aes(long, lat)) +
-  facet_wrap(~ species, scales = 'free') +
-  geom_point() +
-  labs(x = NULL, y = NULL)
+plot_grid(plotlist = map(uds$species, function(.sp) {
+  ggplot() +
+    geom_sf(aes(geometry = x), unnest(filter(uds, species == .sp), akdes))+
+    geom_point(aes(long, lat), filter(unique_locs_spp, species == .sp)) +
+    labs(x = NULL, y = NULL, title = .sp)
+}))
 
 gc() # clean up
 
-temps <- filter(temps,
-                long >= min(unique_locs_spp$long),
-                long <= max(unique_locs_spp$long),
-                lat >= min(unique_locs_spp$lat),
-                lat <= max(unique_locs_spp$lat))
+# extract temperatures for each species
+temps <- readRDS('data/weather-projections.rds') %>%
+  # cut the dataset size in half by filtering to the observed ranges in BC
+  filter(long >= min(unique_locs_spp$long),
+         long <= max(unique_locs_spp$long),
+         lat >= min(unique_locs_spp$lat),
+         lat <= max(unique_locs_spp$lat))
 
 map(unique(unique_locs_spp$species), \(.s) {
-  .u <- unique_locs_spp %>%
-    filter(species == .s)
-
+  # find the locations for the species of interest
+  .u <- filter(unique_locs_spp, species == .s)
+  
+  # filter to the extent of the species for faster wrangling
   .t <- filter(temps,
                long >= min(.u$long),
                long <= max(.u$long),
                lat >= min(.u$lat),
                lat <= max(.u$lat))
-
-  .u %>%
-    left_join(.t, by = c('long', 'lat')) %>%
+  
+  # only keep the locations within each species set of 99.9% UDs
+  left_join(.u, .t, by = c('long', 'lat')) %>%
     saveRDS(paste0('data/weather-projections-', .s, '.rds'))
   
   return(as.character(.s))
