f5f1392bfb11166b5da13e1e0b1a3f56a9c0ef7c
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-08-20 13:00:12 -0700

    updated preds with fixed temperature projections

--- analysis/figures/temperature-hgams-projections.R
+++ analysis/figures/temperature-hgams-projections.R
@@ -92,36 +92,6 @@ if(file.exists('data/cc-hgam-projections.rds')) {
              factor())
   cc_proj
   
-  # sanity check
-  if(FALSE) {
-    ggplot(cc_proj, aes(year, p_median, group = scenario)) +
-      facet_wrap(~ species, scales = 'free') +
-      geom_ribbon(aes(year, ymin = p_lwr_05, ymax = p_upr_95,
-                      fill = scenario), alpha = 0.2) +
-      geom_hline(aes(yintercept = p_ref), lty = 'dashed') +
-      geom_line(color = 'black', lwd = 1.5) +
-      geom_line(aes(color = scenario), lwd = 1) +
-      scale_color_brewer('Climate change scenario', type = 'div',
-                         palette = 5, direction = -1,
-                         aesthetics = c('color', 'fill'))
-    ggplot(cc_proj) +
-      facet_wrap(~ species, scales = 'free') +
-      geom_ribbon(aes(year, ymin = s_lwr_05, ymax = s_upr_95,
-                      fill = scenario), alpha = 0.2) +
-      geom_line(aes(year, s_median, color = scenario)) +
-      scale_color_brewer('Climate change scenario', type = 'div',
-                         palette = 5, direction = -1,
-                         aesthetics = c('color', 'fill'))
-    ggplot(cc_proj) +
-      facet_wrap(~ species, scales = 'free') +
-      geom_ribbon(aes(year, ymin = d_lwr_05, ymax = d_upr_95,
-                      fill = scenario), alpha = 0.2) +
-      geom_line(aes(year, d_median, color = scenario)) +
-      scale_color_brewer('Climate change scenario', type = 'div',
-                         palette = 5, direction = -1,
-                         aesthetics = c('color', 'fill'))
-  }
-  
   saveRDS(cc_proj, 'data/cc-hgam-projections.rds')
   beepr::beep(2)
 }
@@ -129,7 +99,6 @@ if(file.exists('data/cc-hgam-projections.rds')) {
 # make figures ----
 p_p_mov <-
   ggplot(cc_proj, aes(year, p_median, group = scenario)) +
-  coord_cartesian(ylim = c(0.75, 1.25)) +
   facet_wrap(~ species, scales = 'fixed', drop = FALSE,
              labeller = label_parsed) +
   geom_hline(aes(yintercept = 1), lty = 'dashed') +
@@ -144,13 +113,12 @@ p_p_mov <-
   labs(x = NULL, y = 'Relative change in annual P(moving)') +
   scale_x_continuous(breaks = c(2025, 2050, 2075, 2100)) +
   theme(legend.position = 'inside',
-        legend.position.inside = c(5/6, 1/6)); p_p_mov
+        legend.position.inside = c(5/6, 1/6))
 ggsave('figures/p-moving-local-cc-predictions.png', p_p_mov,
        width = 10, height = 6.67, dpi = 600, bg = 'white')
 
 p_s <-
   ggplot(cc_proj, aes(year, s_median, group = scenario)) +
-  coord_cartesian(ylim = c(NA, 1.021)) +
   facet_wrap(~ species, scales = 'fixed', drop = FALSE,
              labeller = label_parsed) +
   geom_ribbon(aes(ymin = s_lwr_05, ymax = s_upr_95,
@@ -165,13 +133,12 @@ p_s <-
   labs(x = NULL, y = 'Relative change in annual speed when moving') +
   scale_x_continuous(breaks = c(2025, 2050, 2075, 2100)) +
   theme(legend.position = 'inside',
-        legend.position.inside = c(5/6, 1/6)); p_s
+        legend.position.inside = c(5/6, 1/6))
 ggsave('figures/speed-local-cc-predictions.png', p_s,
        width = 10, height = 6.67, dpi = 600, bg = 'white')
 
 p_d <-
   ggplot(cc_proj, aes(year, d_median, group = scenario)) +
-  coord_cartesian(ylim = c(0.75, 1.255)) +
   facet_wrap(~ species, scales = 'fixed', drop = FALSE,
              labeller = label_parsed) +
   geom_ribbon(aes(ymin = d_lwr_05, ymax = d_upr_95,
